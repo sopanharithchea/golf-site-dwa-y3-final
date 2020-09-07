c29dd495c7786c89a184dd5b80f44058e0af1764
Sopanharith Chea <csopanharith.student@puthisastra.edu.kh>
2020-09-07 18:26:05 +0700

    Yorn & Teng tasks + News/Article pages

---
+++ Cambo_Golf_resort/resources/views/pages/service.blade.php
@@ -0,0 +1,211 @@
+@extends('layouts.app')
+
+
+@section('content')
+<section class="wpm_title_ber">
+   
+   <div class="container">
+   
+       <div class="row">
+       
+           <div class="col-sm-12">
+           
+               <h2>Services</h2>
+               
+               <div class="ber_link text-center">
+               
+                   <p> <i class="fa fa-angle-double-right"></i> <a href="{{url('/')}}">Home</a> / <a href="{{url('service')}}">Services</a>  <i class="fa fa-angle-double-left"></i> </p>
+               
+              </div>
+           
+           </div>
+       
+       </div>
+   
+   </div>   
+
+</section>
+<section class="service_area">
+
+<div class="wpm_bg_opacity">
+
+      <div class="container">
+   
+       <div class="row">
+       
+           <h2>services</h2>
+           
+           <h3 class="sub_titel">We give you the best service</h3>
+       
+           <div class="col-sm-3">
+               
+               <i class="fa fa-lightbulb-o"></i>
+           
+               <h4>Creative Thinking</h4>
+               
+               <h5>We building creative solutions</h5>
+               
+               <p>In most people's vocabularies, design means veneer. It's interior decorating. It's the fabric of the curtains of the sofa. But to me, nothing could be further from the meaning of design. Design is the fundamental soul of a human-made creation that ends up expressing itself in successive outer layers of the product or service.</p>
+               
+               <button type="button" class="btn btn-default btn-lg wpm_batton wpm_read_btn">Read more</button>
+           
+           </div>
+           
+           <div class="col-sm-3">
+               
+               <i class="fa fa-lightbulb-o"></i>
+           
+               <h4>Creative Thinking</h4>
+               
+               <h5>We building creative solutions</h5>
+               
+               <p>In most people's vocabularies, design means veneer. It's interior decorating. It's the fabric of the curtains of the sofa. But to me, nothing could be further from the meaning of design. Design is the fundamental soul of a human-made creation that ends up expressing itself in successive outer layers of the product or service.</p>
+               
+               <button type="button" class="btn btn-default btn-lg wpm_batton wpm_read_btn">Read more</button>
+           
+           </div>
+           
+           
+           <div class="col-sm-3">
+               
+               <i class="fa fa-lightbulb-o"></i>
+           
+               <h4>Creative Thinking</h4>
+               
+               <h5>We building creative solutions</h5>
+               
+               <p>In most people's vocabularies, design means veneer. It's interior decorating. It's the fabric of the curtains of the sofa. But to me, nothing could be further from the meaning of design. Design is the fundamental soul of a human-made creation that ends up expressing itself in successive outer layers of the product or service.</p>
+               
+               <button type="button" class="btn btn-default btn-lg wpm_batton wpm_read_btn">Read more</button>
+           
+           </div>
+           
+           
+           <div class="col-sm-3">
+               
+               <i class="fa fa-lightbulb-o"></i>
+           
+               <h4>Creative Thinking</h4>
+               
+               <h5>We building creative solutions</h5>
+               
+               <p>In most people's vocabularies, design means veneer. It's interior decorating. It's the fabric of the curtains of the sofa. But to me, nothing could be further from the meaning of design. Design is the fundamental soul of a human-made creation that ends up expressing itself in successive outer layers of the product or service.</p>
+               
+               <button type="button" class="btn btn-default btn-lg wpm_batton wpm_read_btn">Read more</button>
+               
+           
+           </div>
+       
+       </div>
+   
+   </div>
+
+ </div>  
+
+</section>
+<section class="call_us_area">
+
+   <div class="container">
+   
+       <div class="row">
+       
+           <div class="col-sm-9">
+           
+                  <h3>Call us today at <b>+8801599702974</b> or Email us at <a href="#">info@DewThemes.Com</a></h3>
+           
+                   <p>We design and develop the best website's for customers! Order now to get a world class website design.</p>
+           
+           </div>
+           
+           <div class="col-sm-3">
+           
+               <input type="submit" class="wpm_btn wpm_contact_btn" value="CONTACT US">	
+           
+           </div>
+       
+       </div>
+   
+   </div>
+
+</section>
+<section class="wpm_service_area">
+
+      <div class="container">
+   
+       <div class="row">
+       
+           <h2>More Services</h2>
+           
+          <div class="wpm_border center-block"> <i class="fa fa-star-half-o"></i> </div> 
+       
+           <div class="col-sm-4 border_bottom">
+           
+               <i class="fa fa-diamond"></i>
+               
+               <div class="bold_border"></div>
+               
+               <h3>HTML5 AND CSS3</h3>
+           
+           </div>
+           
+           <div class="col-sm-4 border_left_right border_bottom">
+           
+               <i class="fa fa-dashcube"></i>
+               
+               <div class="bold_border"></div>
+               
+               <h3>HTML5 AND CSS3</h3>
+           
+           </div>
+           
+           <div class="col-sm-4 border_bottom">
+           
+               <i class="fa fa-skyatlas"></i>
+               
+               <div class="bold_border"></div>
+               
+               <h3>HTML5 AND CSS3</h3>
+           
+           </div>
+       
+       </div>
+       
+       
+       <div class="row">
+       
+           <div class="col-sm-4">
+           
+               <i class="fa fa-diamond"></i>
+               
+               <div class="bold_border"></div>
+               
+               <h3>HTML5 AND CSS3</h3>
+           
+           </div>
+           
+           <div class="col-sm-4 border_left_right">
+           
+               <i class="fa fa-dashcube"></i>
+               
+               <div class="bold_border"></div>
+               
+               <h3>HTML5 AND CSS3</h3>
+           
+           </div>
+           
+           <div class="col-sm-4 ">
+           
+               <i class="fa fa-skyatlas"></i>
+               
+               <div class="bold_border"></div>
+               
+               <h3>HTML5 AND CSS3</h3>
+           
+           </div>
+       
+       </div>
+   
+   </div>
+
+</section>
+@endsection
