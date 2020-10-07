ba73eef49f2439867714a1f9ce3b787332c8c7b9
Sopanharith Chea <csopanharith.student@puthisastra.edu.kh>
2020-10-07 18:48:16 +0700

    Changes in assignments

--- Cambo_Golf_resort/resources/views/pages/reservation.blade.php
+++ Cambo_Golf_resort/resources/views/pages/reservation.blade.php
@@ -34,9 +34,33 @@
                 <h3>Reservation Form</h3>
 
                 <form>
-
-                    <textarea class="massage_area wpm_textarea" cols="55" rows="10" placeholder="Massage"> </textarea>
-
+                    <div class="input input--nao">
+                        <div>
+                            <label class="input__label-content input__label-content--nao" for="firstName">First
+                                Name</label>
+                            <input class="input__field input__field--nao" type="text" name="firstName" id="firstName">
+                        </div>
+                        <div>
+                            <label class="input__label-content input__label-content--nao" for="lastName">Last
+                                Name</label>
+                            <input class="input__field input__field--nao" type="text" name="lastName" id="lastName">
+                        </div>
+                        <div>
+                            <label class="input__label-content input__label-content--nao" for="email">Email</label>
+                            <input class="input__field input__field--nao" type="text" name="email" id="email">
+                        </div>
+                        <div>
+                            <label class="input__label-content input__label-content--nao" for="startTime">Starting
+                                Time</label>
+                            <input class="input__field input__field--nao" type="text" name="startTime" id="startTime">
+                        </div>
+                        <div>
+                            <label class="input__label-content input__label-content--nao" for="endTime">Ending
+                                Time</label>
+                            <input class="input__field input__field--nao" type="text" name="endTime" id="endTime">
+                        </div>
+                    </div>
+                    <br><br><br>
                     <input class="wpm_sub_btn" type="submit" value="Make Reservation">
 
                 </form>
